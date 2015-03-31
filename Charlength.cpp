0f8647a1fe18372d4c3da33fafc15f78f90954ce
pierrot-8 <pierrot-do8@yahoo.co.jp>
2015-03-31 12:29:33 +0900

    count the number of character string lengthFunction

---
+++ Charlength.cpp
@@ -0,0 +1,28 @@
+//====•¶š—ñ‚Ì”‚ğ’²‚×‚é====
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int strLength(const char*s){
+	int num = 0;
+
+	while (*s++){
+		num++;
+	}
+	return num;
+}
+
+int main(){
+	int num;
+
+	char str[] = "123456789 123456789";
+	
+	cout << "•¶š‚Í=" << str << endl;
+	cout << "•¶š”‚Í=" << strLength(str) << endl;
+
+	getchar();
+
+	return 0;
+}
